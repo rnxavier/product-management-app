a84d6a387d0668e24b4d98af9bd0a59f5bab8fa8
Richelle Xavier <richelle.xavier@outlook.com>
2023-01-06 09:17:17 +0000

    made product list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'pm-root',
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'product-management-app';
+  pageTitle: string = "RJDevs Product Management"
 }
