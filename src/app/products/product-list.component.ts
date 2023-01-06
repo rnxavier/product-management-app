b816e57f055894b6fdfeba9ca842f7b60cac3380
Richelle Xavier <richelle.xavier@outlook.com>
2023-01-06 11:06:59 +0000

    added functionality to show image button

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,35 @@ import { Component } from "@angular/core";
 })
 
 export class ProductListComponent {
-    
+    pageTitle: string = "RJDevs Product Management";
+    imageWidth: number = 100;
+    showImage: boolean = false;
+
+    products: any[] = [
+        {
+        "productId": 2,
+        "productName": "iPad",
+        "productCode": "i02-00234",
+        "releaseDate": "March 18, 2022",
+        "description": "Latest release tablet",
+        "price": 849.99,
+        "starRating": 4.7,
+        "imageURL": "../assets/ipad.jpeg"
+    },
+    {
+        "productId": 5,
+        "productName": "iMac",
+        "productCode": "i20-00487",
+        "releaseDate": "June 5, 2021",
+        "description": "Desktop monitor with built-in PC",
+        "price": 1299,
+        "starRating": 4.9,
+        "imageURL": "../assets/imac.jpeg"
+    }
+];
+
+toggleImage(): void {
+    this.showImage = !this.showImage  
+}
+
 }
